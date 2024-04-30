e3444369623c67a21cd38fe1c5b649b2cd691778
ursche <urjalapariyar@gmail.com>
2024-04-30 16:57:32 +0000

    initial commit

---
+++ rust5.rs
@@ -0,0 +1,18 @@
+//datatype
+fn main() {
+    let x : i32 = 5;  //positive and negative integer
+    println!("{x}");
+
+    let x : f32 = 66.09;   //floating number
+    println!("{x}");
+
+    let x : u32 = 6;     //only positive
+    println!("{x}");
+
+    let x : char = 'b';    //character
+    println!("{x}");
+
+    let x : bool = true;  //boolean either true or false
+    println!("{x}");
+     
+}
